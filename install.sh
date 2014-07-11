4ba83878bc67015a11b040bfc7beaecf609ec4fa
Josh Rendek <josh@bluescripts.net>
2014-07-11 08:33:57 -0400

    update install shell to point to log dir adn remove update api key instruction

--- install.sh
+++ install.sh
@@ -11,10 +11,11 @@ echo "+ Downloading python package reader"
 wget -q https://github.com/joshrendek/sysward-agent/releases/download/1.0/trex.py
 
 chmod +x sysward
-echo "+ Moving config into place, please edit config.json and enter your API key"
+echo "+ Moving config into place"
 mv config.deploy.json config.json
 sed -i "s/APIKEY/$API_KEY/g" config.json
 echo "+ Installing upstart config"
 mv sysward-agent.deploy.conf /etc/init/sysward.conf
 echo "+ Running 'start sysward' to start agent"
 start sysward
+echo "+ Logfiles are located at /var/log/upstart/sysward.log"
