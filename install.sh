c1681341520ea496480bb9a7b29d7f90727b1974
Josh Rendek <josh@bluescripts.net>
2014-07-10 22:52:37 -0400

    installer update

--- install.sh
+++ install.sh
@@ -2,15 +2,18 @@
 mkdir -p /opt/sysward/bin
 cd /opt/sysward/bin
 echo "+ Downloading agent"
-wget https://github.com/joshrendek/sysward-agent/releases/download/1.0/sysward
+wget -q https://github.com/joshrendek/sysward-agent/releases/download/1.0/sysward
 echo "+ Downloading anget config"
-wget https://github.com/joshrendek/sysward-agent/releases/download/1.0/config.deploy.json
+wget -q https://github.com/joshrendek/sysward-agent/releases/download/1.0/config.deploy.json
 echo "+ Downloading upstart config"
-wget https://github.com/joshrendek/sysward-agent/releases/download/1.0/sysward-agent.deploy.conf
+wget -q https://github.com/joshrendek/sysward-agent/releases/download/1.0/sysward-agent.deploy.conf
 echo "+ Downloading python package reader"
-wget https://github.com/joshrendek/sysward-agent/releases/download/1.0/trex.py
+wget -q https://github.com/joshrendek/sysward-agent/releases/download/1.0/trex.py
 
 chmod +x sysward
+echo "+ Moving config into place, please edit config.json and enter your API key"
 mv config.deploy.json config.json
+echo "+ Installing upstart config"
 mv sysward-agent.deploy.conf /etc/init/sysward.conf
+echo "+ Running 'start sysward' to start agent"
 start sysward
